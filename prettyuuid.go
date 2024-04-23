97d04d6848ddfec13b6ae9ed697c934eb29139d2
Ulysse Carion <ulyssecarion@gmail.com>
2024-04-23 10:23:15 -0700

    document, test errors, fuzz

--- prettyuuid.go
+++ prettyuuid.go
@@ -1,3 +1,4 @@
+// Package prettyuuid formats UUIDs with custom alphabets and prefixes.
 package prettyuuid
 
 import (
@@ -7,48 +8,94 @@ import (
 	"strings"
 )
 
+// Format converts between UUIDs and their pretty formats.
+//
+// A Format keeps track of a prefix and an alphabet. The alphabet is used as the set of digits in a base-len(alphabet)
+// numeral system. The prefix is prepended to all formatted strings, and expected in all parsed strings.
+//
+// For example, here a few common alphabets:
+//
+//	binary: 01
+//	hexadecimal: 0123456789abcdef
+//	raw unpadded base64: ABCDEFGHJIKLMNOPQRSTUVWYZabcdefghijklmnopqrstuvwxyz0123456789+/
 type Format struct {
-	Prefix   string
-	Alphabet string
+	prefix   string
+	alphabet string
 }
 
+// NewFormat creates a Format with the given prefix and alphabet.
+//
+// For example, to create a Format where UUIDs are prefixed with "invoice_" and are base36-encoded, use:
+//
+//	NewFormat("invoice_", "0123456789abcdefghijklmnopqrstuvwxyz")
+//
+// NewFormat returns an error if len(alphabet) < 2 or if alphabet contains duplicate characters. Any prefix, including
+// an empty prefix, is valid.
+func NewFormat(prefix, alphabet string) (Format, error) {
+	if len(alphabet) < 2 {
+		return Format{}, fmt.Errorf("alphabet must have len >= 2: %q", alphabet)
+	}
+	seen := map[byte]struct{}{}
+	for i := 0; i < len(alphabet); i++ {
+		if _, ok := seen[alphabet[i]]; ok {
+			return Format{}, fmt.Errorf("alphabet must not contain duplicate chars: %q", alphabet)
+		}
+		seen[alphabet[i]] = struct{}{}
+	}
+	return Format{prefix: prefix, alphabet: alphabet}, nil
+}
+
+// MustNewFormat is like NewFormat but panics if alphabet isn't valid.
+func MustNewFormat(prefix, alphabet string) Format {
+	format, err := NewFormat(prefix, alphabet)
+	if err != nil {
+		panic(fmt.Errorf("NewFormat(%q, %q) err: %w", prefix, alphabet, err))
+	}
+	return format
+}
+
+// Format converts a UUID to a pretty string.
 func (f *Format) Format(uuid [16]byte) string {
+	s := make([]byte, f.len())
+	copy(s, f.prefix)
+	for i := len(f.prefix); i < f.len(); i++ {
+		s[i] = f.alphabet[0]
+	}
+
 	var n big.Int
 	n.SetBytes(uuid[:])
 
-	b := big.NewInt(int64(len(f.Alphabet)))
-
-	s := make([]byte, f.len())
-	copy(s, f.Prefix)
-	for i := len(f.Prefix); i < f.len(); i++ {
-		s[i] = f.Alphabet[0]
-	}
-
+	b := big.NewInt(int64(len(f.alphabet)))
 	i := len(s) - 1
 	for n.BitLen() > 0 {
 		var r big.Int
 		n.QuoRem(&n, b, &r)
 
-		s[i] = f.Alphabet[r.Int64()]
+		s[i] = f.alphabet[r.Int64()]
 		i--
 	}
 
 	return string(s)
 }
 
+// Parse converts a pretty string to a UUID.
 func (f *Format) Parse(s string) ([16]byte, error) {
 	if len(s) != f.len() {
 		return [16]byte{}, fmt.Errorf("%q does not have expected length %v", s, f.len())
 	}
 
+	if !strings.HasPrefix(s, f.prefix) {
+		return [16]byte{}, fmt.Errorf("%q does not have expected prefix %q", s, f.prefix)
+	}
+
 	var n big.Int
-	for i := len(f.Prefix); i < len(s); i++ {
-		d := strings.IndexByte(f.Alphabet, s[i])
+	for i := len(f.prefix); i < len(s); i++ {
+		d := strings.IndexByte(f.alphabet, s[i])
 		if d == -1 {
 			return [16]byte{}, fmt.Errorf("%q contains illegal char at position %v", s, i)
 		}
 
-		n.Mul(&n, big.NewInt(int64(len(f.Alphabet))))
+		n.Mul(&n, big.NewInt(int64(len(f.alphabet))))
 		n.Add(&n, big.NewInt(int64(d)))
 	}
 
@@ -65,5 +112,5 @@ func (f *Format) Parse(s string) ([16]byte, error) {
 func (f *Format) len() int {
 	// digits required is ceil(log_{radix}(max_uuid))
 	// log_{radix}(max_uuid) = log2(max_uuid) / log2(radix) = 128 / log2(radix)
-	return len(f.Prefix) + int(math.Ceil(128.0/math.Log2(float64(len(f.Alphabet)))))
+	return len(f.prefix) + int(math.Ceil(128.0/math.Log2(float64(len(f.alphabet)))))
 }
--- prettyuuid_test.go
+++ prettyuuid_test.go
@@ -1,6 +1,7 @@
 package prettyuuid_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -8,7 +9,85 @@ import (
 	"github.com/ssoready/prettyuuid"
 )
 
-func TestFormat(t *testing.T) {
+func Example() {
+	format := prettyuuid.MustNewFormat("invoice_", "0123456789abcdefghijklmnopqrstuvwxyz")
+
+	fmt.Println(format.Format(uuid.MustParse("f81d4fae-7dec-11d0-a765-00a0c91e6bf6")))
+
+	id, _ := format.Parse("invoice_eoswzolg3bsx0zn8otq1p8oom")
+	fmt.Println(uuid.UUID(id).String())
+	// Output:
+	// invoice_eoswzolg3bsx0zn8otq1p8oom
+	// f81d4fae-7dec-11d0-a765-00a0c91e6bf6
+}
+
+func TestNewFormat(t *testing.T) {
+	testCases := []struct {
+		Alphabet string
+		WantErr  string
+	}{
+		{
+			Alphabet: "",
+			WantErr:  `alphabet must have len >= 2: ""`,
+		},
+		{
+			Alphabet: "a",
+			WantErr:  `alphabet must have len >= 2: "a"`,
+		},
+		{
+			Alphabet: "aa",
+			WantErr:  `alphabet must not contain duplicate chars: "aa"`,
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(fmt.Sprintf("%q", tt.Alphabet), func(t *testing.T) {
+			_, err := prettyuuid.NewFormat("", tt.Alphabet)
+			if d := cmp.Diff(tt.WantErr, err.Error()); d != "" {
+				t.Fatalf("NewFormat(%v) did not return expected err (-want +got):\n%s", tt.Alphabet, d)
+			}
+		})
+	}
+}
+
+func TestFormat_Parse(t *testing.T) {
+	testCases := []struct {
+		Name    string
+		Format  prettyuuid.Format
+		ID      string
+		WantErr string
+	}{
+		{
+			Name:    "bad length",
+			Format:  prettyuuid.MustNewFormat("", "0123456789abcdef"),
+			ID:      "00000000000000000000000000000000x",
+			WantErr: `"00000000000000000000000000000000x" does not have expected length 32`,
+		},
+		{
+			Name:    "bad prefix",
+			Format:  prettyuuid.MustNewFormat("prefix_", "0123456789abcdefghijklmnopqrstuvwxyz"),
+			ID:      "xxxxxx_0000000000000000000000000",
+			WantErr: `"xxxxxx_0000000000000000000000000" does not have expected prefix "prefix_"`,
+		},
+		{
+			Name:    "bad char",
+			Format:  prettyuuid.MustNewFormat("", "0123456789abcdef"),
+			ID:      "0000000000000000000000000000000x",
+			WantErr: `"0000000000000000000000000000000x" contains illegal char at position 31`,
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.Name, func(t *testing.T) {
+			_, err := tt.Format.Parse(tt.ID)
+			if d := cmp.Diff(tt.WantErr, err.Error()); d != "" {
+				t.Fatalf("%v.Parse(%v) did not return expected err (-want +got):\n%s", tt.Format, tt.ID, d)
+			}
+		})
+	}
+}
+
+func TestFormat_roundtrip(t *testing.T) {
 	testCases := []struct {
 		Name   string
 		Format prettyuuid.Format
@@ -17,73 +96,73 @@ func TestFormat(t *testing.T) {
 	}{
 		{
 			Name:   "round-trip zero uuid",
-			Format: prettyuuid.Format{Alphabet: "0123456789abcdef"},
+			Format: prettyuuid.MustNewFormat("", "0123456789abcdef"),
 			UUID:   "00000000-0000-0000-0000-000000000000",
 			Want:   "00000000000000000000000000000000",
 		},
 		{
 			Name:   "round-trip one uuid",
-			Format: prettyuuid.Format{Alphabet: "0123456789abcdef"},
+			Format: prettyuuid.MustNewFormat("", "0123456789abcdef"),
 			UUID:   "00000000-0000-0000-0000-000000000001",
 			Want:   "00000000000000000000000000000001",
 		},
 		{
 			Name:   "round-trip max uuid",
-			Format: prettyuuid.Format{Alphabet: "0123456789abcdef"},
+			Format: prettyuuid.MustNewFormat("", "0123456789abcdef"),
 			UUID:   "ffffffff-ffff-ffff-ffff-ffffffffffff",
 			Want:   "ffffffffffffffffffffffffffffffff",
 		},
 		{
 			Name:   "round-trip sample uuid",
-			Format: prettyuuid.Format{Alphabet: "0123456789abcdef"},
+			Format: prettyuuid.MustNewFormat("", "0123456789abcdef"),
 			UUID:   "f81d4fae-7dec-11d0-a765-00a0c91e6bf6",
 			Want:   "f81d4fae7dec11d0a76500a0c91e6bf6",
 		},
 		{
 			Name:   "alphanumeric zero uuid",
-			Format: prettyuuid.Format{Alphabet: "0123456789abcdefghijklmnopqrstuvwxyz"},
+			Format: prettyuuid.MustNewFormat("", "0123456789abcdefghijklmnopqrstuvwxyz"),
 			UUID:   "00000000-0000-0000-0000-000000000000",
 			Want:   "0000000000000000000000000",
 		},
 		{
 			Name:   "alphanumeric one uuid",
-			Format: prettyuuid.Format{Alphabet: "0123456789abcdefghijklmnopqrstuvwxyz"},
+			Format: prettyuuid.MustNewFormat("", "0123456789abcdefghijklmnopqrstuvwxyz"),
 			UUID:   "00000000-0000-0000-0000-000000000001",
 			Want:   "0000000000000000000000001",
 		},
 		{
 			Name:   "alphanumeric max uuid",
-			Format: prettyuuid.Format{Alphabet: "0123456789abcdefghijklmnopqrstuvwxyz"},
+			Format: prettyuuid.MustNewFormat("", "0123456789abcdefghijklmnopqrstuvwxyz"),
 			UUID:   "ffffffff-ffff-ffff-ffff-ffffffffffff",
 			Want:   "f5lxx1zz5pnorynqglhzmsp33",
 		},
 		{
 			Name:   "alphanumeric sample uuid",
-			Format: prettyuuid.Format{Alphabet: "0123456789abcdefghijklmnopqrstuvwxyz"},
+			Format: prettyuuid.MustNewFormat("", "0123456789abcdefghijklmnopqrstuvwxyz"),
 			UUID:   "f81d4fae-7dec-11d0-a765-00a0c91e6bf6",
 			Want:   "eoswzolg3bsx0zn8otq1p8oom",
 		},
 		{
 			Name:   "alphanumeric zero uuid with prefix",
-			Format: prettyuuid.Format{Prefix: "prefix_", Alphabet: "0123456789abcdefghijklmnopqrstuvwxyz"},
+			Format: prettyuuid.MustNewFormat("prefix_", "0123456789abcdefghijklmnopqrstuvwxyz"),
 			UUID:   "00000000-0000-0000-0000-000000000000",
 			Want:   "prefix_0000000000000000000000000",
 		},
 		{
 			Name:   "alphanumeric one uuid with prefix",
-			Format: prettyuuid.Format{Prefix: "prefix_", Alphabet: "0123456789abcdefghijklmnopqrstuvwxyz"},
+			Format: prettyuuid.MustNewFormat("prefix_", "0123456789abcdefghijklmnopqrstuvwxyz"),
 			UUID:   "00000000-0000-0000-0000-000000000001",
 			Want:   "prefix_0000000000000000000000001",
 		},
 		{
 			Name:   "alphanumeric max uuid with prefix",
-			Format: prettyuuid.Format{Prefix: "prefix_", Alphabet: "0123456789abcdefghijklmnopqrstuvwxyz"},
+			Format: prettyuuid.MustNewFormat("prefix_", "0123456789abcdefghijklmnopqrstuvwxyz"),
 			UUID:   "ffffffff-ffff-ffff-ffff-ffffffffffff",
 			Want:   "prefix_f5lxx1zz5pnorynqglhzmsp33",
 		},
 		{
 			Name:   "alphanumeric sample uuid with prefix",
-			Format: prettyuuid.Format{Prefix: "prefix_", Alphabet: "0123456789abcdefghijklmnopqrstuvwxyz"},
+			Format: prettyuuid.MustNewFormat("prefix_", "0123456789abcdefghijklmnopqrstuvwxyz"),
 			UUID:   "f81d4fae-7dec-11d0-a765-00a0c91e6bf6",
 			Want:   "prefix_eoswzolg3bsx0zn8otq1p8oom",
 		},
@@ -107,4 +186,28 @@ func TestFormat(t *testing.T) {
 			}
 		})
 	}
+}
+
+func FuzzFormat_roundtrip(f *testing.F) {
+	f.Fuzz(func(t *testing.T, prefix, alphabet string, fuzzID []byte) {
+		// we have to take a byte slice because go fuzz does not take arrays
+		if len(fuzzID) != 16 {
+			return
+		}
+		id := (*[16]byte)(fuzzID)
+
+		format, err := prettyuuid.NewFormat(prefix, alphabet)
+		if err != nil {
+			return // we don't care about invalid formats
+		}
+
+		formatted := format.Format(*id)
+		parsed, err := format.Parse(formatted)
+		if err != nil {
+			t.Fatalf("%v.Parse(%v) err: %v", format, formatted, err)
+		}
+		if d := cmp.Diff(*id, parsed); d != "" {
+			t.Fatalf("%v.Parse(Format(%v)) does not round-trip: (-want +got)\n%s", format, id, d)
+		}
+	})
 }
